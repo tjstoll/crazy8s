231f7eecda7fd1dcc76a881a8fee96296fb59e28
tjstoll <taneishastoll@gmail.com>
2024-11-11 13:59:20 -0800

    began computer decision

--- crazy8s.py
+++ crazy8s.py
@@ -17,14 +17,22 @@ playCard = ""
 
 # Build the main deck
 cardValues = ['A','2','3','4','5','6','7','8','9','10','J','K','Q']
-cardSuits = ['s', 'c', 'h', 'd']
+cardSuits = [' s', ' c', ' h', ' d']
 deck = [value + suit for value in cardValues for suit in cardSuits]
+
+cardRanking = {
+    'Q s': 4,
+    '2': 3,
+    'J': 2,
+    '8': 1
+    }
 
 
 # Operations ==================================================================
 def shuffle(cards: list):
     """
-    Returns shuffled cards
+    Shuffle indices of cards.
+    Returns shuffled cards.
     """
     cardsLength = len(cards)
     shuffledIndices = random.sample(range(0, cardsLength), cardsLength)
@@ -35,7 +43,8 @@ def shuffle(cards: list):
 
 def deal(hand: list, cards: list, quantity: int):
     """
-    Returns the hand with more cards and cards with less cards
+    Deal quantity from cards and place in hand.
+    Returns updated hand and updated cards.
     """
     # add cards to hand
     hand.extend(cards[:quantity])
@@ -47,6 +56,8 @@ def deal(hand: list, cards: list, quantity: int):
 
 def playCard(playCard: str, hand: list, playDeck: list):
     """
+    Take playCard from hand and place in playDeck.
+    Return updated hand and updated playDeck.
     """
     # add play card to playDeck
     playDeck.append(playCard)
@@ -55,14 +66,36 @@ def playCard(playCard: str, hand: list, playDeck: list):
     
     return hand, playDeck
 
+def computerPlay(opponentHand: list, playDeck: list):
+    """
+    
+    """
+    topCard = playDeck[-1].split(' ')
+    cardsToPlay = []
+    playCard = ''
+    
+    # Collect all cards in hand that can be played on the topCard
+    for card in opponentHand:
+        if topCard[0] in card or topCard[1] in card:
+            cardsToPlay.append(card)       
+    
+    # Decide which is the better card to play
+    if len(cardsToPlay) > 1:
+            pass
+        
+
+
 # Display =====================================================================
 def displayGame():
     """
+    Display current state of the game
     """
-    print('Opp: ', opponentHand) # DELETE THIS!!
-    print(playDeck[-1], len(drawDeck))
+    print('\nOpp: ', opponentHand) # DELETE THIS!!
+    print(playDeck[-1], '|', len(drawDeck))
     print('You: ', playerHand)
     
+    
+# =============================================================================
 if __name__ == "__main__":
     drawDeck = shuffle(deck)
     opponentHand, drawDeck = deal(opponentHand, drawDeck, 8)
